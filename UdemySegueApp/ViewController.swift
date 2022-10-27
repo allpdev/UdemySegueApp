296bdf3fb550450e525f06f7b9f1136b210f1acd
allpdev <alpkctpdev@gmail.com>
2022-10-27 21:00:23 +0300

    Base

---
+++ UdemySegueApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UdemySegueApp
+//
+//  Created by alp on 27.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
